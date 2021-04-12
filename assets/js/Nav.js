420aea624261fb1b467822f385a117acf5b57f78
Noah Varghese <varghese.noah@gmail.com>
2021-04-12 11:55:20 -0400

    Update Nav.js

--- assets/js/Nav.js
+++ assets/js/Nav.js
@@ -1,3 +1,9 @@
+/*
+“StAuth10065: I Noah Varghese, 000753196 certify that this material is my original work. 
+No other person’s work has been used without due acknowledgement. 
+I have not made my work available to anyone else.”
+*/
+
 const Nav = () => {
     return (
         <div>
