ed3394f76e856399829604992d2e31013f9436b6
Noah Varghese <varghese.noah@gmail.com>
2021-04-12 11:54:36 -0400

    Update App.js

--- assets/js/App.js
+++ assets/js/App.js
@@ -1,3 +1,8 @@
+/*
+“StAuth10065: I Noah Varghese, 000753196 certify that this material is my original work. 
+No other person’s work has been used without due acknowledgement. 
+I have not made my work available to anyone else.”
+*/
 const App = () => {
     const [state, setState] = React.useState({
         index: undefined,
